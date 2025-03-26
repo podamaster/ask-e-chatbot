9f3b0fe85b4f1ca188c70f4dfeb996f5d7fc0afb
po <155928024+podamaster@users.noreply.github.com>
2025-03-26 04:17:10 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, () => {
+    console.log(`ASK-E 1.0 is running at http://localhost:${port}`);
+});
